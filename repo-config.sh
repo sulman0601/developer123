296f05f63706ad6b3516215c13be4f8fc0e417bc
L1F20BSSE0601 <L1F20BSSE0601@ucp.com>
2025-01-20 14:22:30 +0500

    initial commit

---
+++ repo-config.sh
@@ -0,0 +1,7 @@
+echo "Going to configure git repo"
+git branch -m develop
+git config --local user.name ""l1f20bsse0601"
+git congig --local user.email "l1f20bsse0601@ucp.edu.pk"
+git config --local core.editor notepad"
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css
